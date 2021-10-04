baa62656b1e0bff9b44232b08841ab31d14ef350
AlexandreLuisSilva <alexandre.luis.silva@gmail.com>
2021-10-04 16:02:08 -0300

    Ultimo com 4 testes passando

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,11 +1,17 @@
 package com.devsuperior.bds02.services;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
 import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.exceptions.DataBaseException;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
@@ -16,11 +22,9 @@ public class EventService {
 	@Autowired
 	private EventRepository repository;
 	
-	/*
 	@Autowired
 	private CityRepository cityRepository; 
-	*/
-	/*
+	
 	@Transactional
 	public EventDTO update(Long id, EventDTO dto) {
 		try {
@@ -32,7 +36,7 @@ public class EventService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
 		}
-	}*/
+	}
 	
 	public void delete(Long id) {
 		try {
@@ -46,18 +50,16 @@ public class EventService {
 		}
 	}
 	
-	/*
 	private void copyDtoToEntity(EventDTO dto, Event entity) {
 	    entity.setName(dto.getName());
 	    entity.setUrl(dto.getUrl());
 	    entity.setDate(dto.getDate());
 	    entity.setCity(new City(dto.getCityId(), null));
 
-		entity.getCity().clear;
 		for (EventDTO citDTO : dto.getCityId()) {
 			City city = cityRepository.getOne(citDTO.getId());
 			entity.getCity().getId();
 		}
-	}*/
+	}
 }
 
