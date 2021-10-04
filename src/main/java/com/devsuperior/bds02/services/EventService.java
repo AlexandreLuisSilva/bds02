3d5993187411e52a6b2f6247412690d65c81840b
AlexandreLuisSilva <alexandre.luis.silva@gmail.com>
2021-10-04 15:38:40 -0300

    4 Testes Passando

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,28 +1,26 @@
 package com.devsuperior.bds02.services;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
 
 import com.devsuperior.bds02.dto.EventDTO;
-import com.devsuperior.bds02.entities.City;
-import com.devsuperior.bds02.entities.Event;
-import com.devsuperior.bds02.repositories.CityRepository;
 import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.exceptions.DataBaseException;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
+@Service
 public class EventService {
 
 	@Autowired
 	private EventRepository repository;
 	
+	/*
 	@Autowired
 	private CityRepository cityRepository; 
-
+	*/
+	/*
 	@Transactional
 	public EventDTO update(Long id, EventDTO dto) {
 		try {
@@ -34,7 +32,7 @@ public class EventService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
 		}
-	}
+	}*/
 	
 	public void delete(Long id) {
 		try {
@@ -48,16 +46,18 @@ public class EventService {
 		}
 	}
 	
-	private void copyDTOToEntity(EventDTO dto, Event entity) {
-		
-		entity.setName(dto.getName());
-		entity.setDate(dto.getDate());
-		
-		entity.getCity().getId();
-		for (EventDTO citDTO : dto.getCity()) {
+	/*
+	private void copyDtoToEntity(EventDTO dto, Event entity) {
+	    entity.setName(dto.getName());
+	    entity.setUrl(dto.getUrl());
+	    entity.setDate(dto.getDate());
+	    entity.setCity(new City(dto.getCityId(), null));
+
+		entity.getCity().clear;
+		for (EventDTO citDTO : dto.getCityId()) {
 			City city = cityRepository.getOne(citDTO.getId());
 			entity.getCity().getId();
 		}
-	}
+	}*/
 }
 
