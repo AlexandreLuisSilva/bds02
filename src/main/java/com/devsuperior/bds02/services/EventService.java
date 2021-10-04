6c96a4093f0c2bec04f8d2cec5790c29744687b8
AlexandreLuisSilva <alexandre.luis.silva@gmail.com>
2021-10-04 17:27:22 -0300

    6 testes passsando

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,17 +1,20 @@
 package com.devsuperior.bds02.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
-import com.devsuperior.bds02.repositories.CityRepository;
 import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.exceptions.DataBaseException;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
@@ -22,6 +25,11 @@ public class EventService {
 	@Autowired
 	private EventRepository repository;
 	
+	public List<EventDTO> findAll() {
+		List<Event> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new EventDTO(x)).collect(Collectors.toList());
+	}
+
 	@Transactional
 	public EventDTO update(Long id, EventDTO dto) {
 		try {
